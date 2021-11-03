48ab141a6c0e4218d0aec89c1675241fb4be367a
nailtonsimoes <nailtonsimoes@gmail.com>
2021-11-03 15:37:29 -0300

    primeiro push pro repositorio

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'crud-7en',
+    appId: '1:1085791139993:web:8f0d7ed2f033335c106008',
+    databaseURL: 'https://crud-7en-default-rtdb.firebaseio.com',
+    storageBucket: 'crud-7en.appspot.com',
+    apiKey: 'AIzaSyB_162oLP9zPo79rifnuZv8FE849GluV8c',
+    authDomain: 'crud-7en.firebaseapp.com',
+    messagingSenderId: '1085791139993',
+    measurementId: 'G-LW4RJZSJKP',
+  },
   production: false
 };
 
